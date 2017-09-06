e46bc9ccb8e8bdf1a241ff56ba898d607141289e
Jack Yang <ibcacbi@gmail.com>
2017-09-06 09:05:21 +0800

    ignore

---
+++ src/main/java/Tdd.java
@@ -0,0 +1,6 @@
+/**
+ * Created by jack on 2017/9/6.
+ */
+public class Tdd {
+
+}
---
+++ src/test/java/TddTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by jack on 2017/9/6.
+ */
+public class TddTest {
+    @Test
+    public void should_test_all(){
+    
+    }
+}
